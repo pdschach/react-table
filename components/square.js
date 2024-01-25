bc2513017f62aa4638be144b03b386eeeeb165b4
pdschach <pieterjandeschacht@hotmail.com>
2024-01-25 12:50:42 +0100

    voor copy tictactoe

---
+++ components/square.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Square = ( {value, onSquareClick} ) => {
+
+   
+    return(
+        
+        <button 
+        className="square"
+        onClick={onSquareClick}
+        >
+            {value}
+        </button>
+    )
+};
+
+export default Square;
+
